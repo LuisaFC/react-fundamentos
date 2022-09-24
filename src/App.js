7980d5b13456331d7eba2abdf27148024933f8da
LuisaFC <luisabsb@gmail.com>
2022-09-24 17:58:17 -0300

    PropTypes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Header from './Header'
 function App(){
   return (
     <>
-      <Header title="JStack's Blog"> 
+      <Header> 
         <h2>Posts da semana</h2>
       </Header>
 
       <hr />
 
       <Post 
+        likes={20}
         post={{
           title: 'Posts da semana 01',
           subtitle: 'Posts da semana 01'
         }}
       />
 
-<Post 
+      <Post 
+        likes={10}
         post={{
           title: 'Posts da semana 02',
           subtitle: 'Posts da semana 02'
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Header({title, children}){
   return(
@@ -7,4 +8,13 @@ export default function Header({title, children}){
       {children}
     </>
   )
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+Header.defaultProps = {
+  title: `JStack's Blog`
 }
